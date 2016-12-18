1f9387045431cb2626a3d0d286aa43f449483de5
Alex <rutol@mail.ru>
2016-12-18 14:08:04 +0300

    Revert "Задание 13. Работа с корзиной"
    This reverts commit c337363305fce8952d5f26986ee14b177cc2c55c.

--- java-example/src/Test/java/ru/stqa/training/selenium/CheckCart.java
+++ java-example/src/Test/java/ru/stqa/training/selenium/CheckCart.java
@@ -1,6 +1,7 @@
 package ru.stqa.training.selenium;
 
 import org.junit.After;
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.By;
@@ -23,8 +24,8 @@ public class CheckCart {
     private WebDriver driver;
     private WebDriverWait wait;
     List<WebElement> prodList;
-    WebElement productUnit, Cart, prodTable;
-    int i, j, k, k1, p;
+    WebElement productUnit, Cart, cartQuantity, prodTable;
+    int prodQuantity, i, j, k, k1, p, cartQuantityBefore, cartQuantityAfter;
     String[] prodName;
 
 
@@ -77,7 +78,12 @@ public class CheckCart {
             productUnit.click(); //щелкаем по странице продукта
             wait.until(titleContains(prodName[i]));
 
-             Cart = wait.until(presenceOfElementLocated(By.id("cart"))); // нашли корзину
+            // WebElement element = wait.until(presenceOfElementLocated(By.name("q")));
+            //prCart = driver.findElement(By.id("cart")); // нашли корзину
+            Cart = wait.until(presenceOfElementLocated(By.id("cart"))); // нашли корзину
+            //cartQuantity = Cart.findElement(By.cssSelector("span.quantity"));
+            // указатель количества товаров в корзине
+            //cartQuantityBefore = Integer.parseInt(cartQuantity.getText());
 
             k=prodName[i].compareToIgnoreCase("Yellow Duck");
             // Проверяем, что выбранный товар не Yellow Duck - требует доп. обработки
@@ -87,10 +93,25 @@ public class CheckCart {
 
             driver.findElement(By.name("add_cart_product")).click();
             // добавляем продукт в корзину
-
-            wait.until(textToBePresentInElement(Cart.findElement(By.cssSelector("span.quantity")),Integer.toString(i+1)));
+            //wait.until(ExpectedConditions.presenceOfElementLocated(By.id("cart")));
+            int k2=i+1;
+            wait.until(textToBePresentInElement(Cart.findElement(By.cssSelector("span.quantity")),Integer.toString(k2)));
             // ждем изменения количества
 
+
+            //Cart = driver.findElement(By.id("cart"));
+                    //wait.until(presenceOfElementLocated(By.id("cart"))); // нашли корзину
+
+            //cartQuantity = Cart.findElement(By.cssSelector("span.quantity"));
+            // находим количество товаров в корзине заново
+
+            //cartQuantityAfter = Integer.parseInt(cartQuantity.getText());
+
+            //wait = new WebDriverWait(driver,10);
+
+            //Assert.assertTrue((cartQuantityAfter-cartQuantityBefore)==1);
+
+            //wait = new WebDriverWait(driver,10);
         }
         driver.get("http://localhost/litecart/en/"); //открыть главную страницу магазина
 
@@ -111,11 +132,13 @@ public class CheckCart {
                  */
             }
 
+            //driver.findElement(By.cssSelector("[name=quantity]")).sendKeys("1");
+
             driver.findElement(By.name("remove_cart_item")).click();
             // кликнуть по кнопке удаления товара Remove
             wait.until(stalenessOf(prodTable));
         }
-
+        //prodList = driver.findElements(By.cssSelector("li.shortcut"));
         driver.get("http://localhost/litecart/en/");
         wait.until(titleContains("Online Store"));
 
